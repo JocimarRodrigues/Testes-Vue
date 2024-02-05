dc6d0f670a1d0c6fff718bbe7ab1c75df341bee3
John Rodrigues <jocimarrodrigues1337@gmail.com>
2024-02-05 10:20:12 -0300

    passando html com computed

--- vue-props/src/components/Props3FormaComponent.vue
+++ vue-props/src/components/Props3FormaComponent.vue
@@ -2,13 +2,18 @@
     <div>
         <h2>{{ nomeCompleto }}</h2>
     </div>
+    <div class="card" v-html="card">
+    </div>
+    <div class="cardFiltro" v-html="cardFiltro"></div>
 </template>
 <script lang="ts">
 
 export default {
     name: 'Props3FormaComponent',
     props: {
-        nomeCompleto: String
+        nomeCompleto: String,
+        card: String,
+        cardFiltro: String
     },
     setup(props) {
         console.log(props.nomeCompleto)
--- vue-props/src/views/Props3FormaView.vue
+++ vue-props/src/views/Props3FormaView.vue
@@ -4,7 +4,7 @@
             <input type="text" name="nome" v-model="nomeInicial">
             <input type="text" name="sobrenome" v-model="nomeFinal">
         </div>
-        <Props3FormaComponent :nomeCompleto="nomeCompleto" />
+        <Props3FormaComponent :nomeCompleto="nomeCompleto"  :card="card" :cardFiltro="cardFiltro"/>
     </div>
 </template>
 <script lang="ts">
@@ -19,18 +19,34 @@ export default {
     setup() {
         const nomeInicial = ref('')
         const nomeFinal = ref('')
+        const nomeFIltrado = ref('')
+
+        const card = computed(() => {
+            return `<h2 style="color: red">${nomeInicial.value} </h2> <h3 style="color: blue">${nomeFinal.value} </3> `
+        })
+
+        const cardFiltro = computed(() => {
+            nomeInicial.value  == 'John' ? nomeFIltrado.value : 'Nome não passou no filtro.' 
+            // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+            nomeFIltrado.value = nomeInicial.value === 'John' ? 'Nome passou no filtro.' : 'Nome não passou no filtro.';
+            // Essa lógica está errada porque uma propriedade computada não deve alterar o valor de um estado.
+            return `<h1>${nomeFIltrado.value} </h1>`
+        })
+
 
 
 
 
         const nomeCompleto = computed(() => { return `${nomeInicial.value}${nomeFinal.value} ` })
 
-        console.log(nomeCompleto)
+        console.log(nomeFIltrado.value)
 
         return {
             nomeInicial,
             nomeFinal,
-            nomeCompleto
+            nomeCompleto,
+            card,
+            cardFiltro
         }
     }
 }
